de77872079ac07be03c4187fb5e1f45804dcacad
alixlm19 <alixlm19@hotmail.com>
2022-10-11 14:30:35 -0400

    Ignore shiny site credentials

--- app/server.R
+++ app/server.R
@@ -5,5 +5,7 @@ server <- function(input, output) {
     output$map_output <- renderLeaflet({
         leaflet_map
     })
+
+    output$map_output <- 
     output$map_label_boroughs <- map_borough_label(input)
 }
