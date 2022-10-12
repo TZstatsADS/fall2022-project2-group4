bc74c5ca3bdcbbbe40059946ee14df4ca49c08a3
alixlm19 <alixlm19@hotmail.com>
2022-10-12 16:11:28 -0400

    Added server integration to plots

--- app/server.R
+++ app/server.R
@@ -13,20 +13,47 @@ server <- function(input, output) {
     output$plot_injuries_per_collision_1 <- renderPlot(plot_injuries_per_collision_1)
     output$plot_death_per_collision_1 <- renderPlot(plot_death_per_collision_1)
 
-    output$plot_volume_2 <- renderPlot(plot_volume_2)
-    output$plot_no_collisions_2 <- renderPlot(plot_no_collisions_2)
-    output$plot_total_injuries_2 <- renderPlot(plot_total_injuries_2)
-    output$plot_total_deaths_2 <- renderPlot(plot_total_deaths_2)
-    output$plot_injuries_per_collision_2 <- renderPlot(plot_injuries_per_collision_2)
-    output$plot_death_per_collision_2 <- renderPlot(plot_death_per_collision_2)
 
-    output$plot_volume_3 <- renderPlot(plot_volume_3)
-    output$plot_no_collisions_3 <- renderPlot(plot_no_collisions_3)
-    output$plot_total_injuries_3 <- renderPlot(plot_total_injuries_3)
-    output$plot_total_deaths_3 <- renderPlot(plot_total_deaths_3)
-    output$plot_injuries_per_collision_3 <- renderPlot(plot_injuries_per_collision_3)
-    output$plot_death_per_collision_3 <- renderPlot(plot_death_per_collision_3)
 
+    # Graphs panel 2
+    output$plot_volume_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_volume_2")
+    })
+    output$plot_no_collisions_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_no_collisions_2")
+    })
+    output$plot_total_injuries_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_total_injuries_2")
+    })
+    output$plot_total_deaths_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_total_deaths_2")
+    })
+    output$plot_injuries_per_collision_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_injuries_per_collision_2")
+    })
+    output$plot_death_per_collision_2 <- renderPlot({
+        get_plots_2(input, plot_name = "plot_death_per_collision_2")
+    })
+
+
+    # Graphs panel 3
+    output$plot_no_collisions_3 <- renderPlot({
+        get_plots_3(input, plot_name = "plot_no_collisions_3")
+    })
+    output$plot_total_injuries_3 <- renderPlot({
+        get_plots_3(input, plot_name = "plot_total_injuries_3")
+    })
+    output$plot_total_deaths_3 <- renderPlot({
+        get_plots_3(input, plot_name = "plot_total_deaths_3")
+    })
+    output$plot_injuries_per_collision_3 <- renderPlot({
+        get_plots_3(input, plot_name = "plot_injuries_per_collision_3")
+    })
+    output$plot_death_per_collision_3 <- renderPlot({
+        get_plots_3(input, plot_name = "plot_death_per_collision_3")
+    })
+
+    
 
     output$map_label_boroughs <- map_borough_label(input)
 }
