10b03ac4b50168182b3a34e1d22ebecbb4410c95
alixlm19 <alixlm19@hotmail.com>
2022-10-12 13:02:21 -0400

    Moved global file to root

---
+++ global.R
@@ -0,0 +1,26 @@
+DEPLOY <- F
+if (!DEPLOY) {
+
+    setwd("./app")
+    source("./dependencies.R")
+    source("./server.R")
+    source("./ui.R")
+    source("./shinyapps_io.R")
+
+    app <- shinyApp(
+        ui,
+        server,
+        options = list(
+            launch.browser = FALSE,
+            autoload.r = TRUE
+        )
+    )
+    runApp(app)
+} else {
+    rsconnect::deployApp(
+        upload = TRUE,
+        appName = "Group4-Applied_Data_Science",
+        appTitle = "Project Oogway",
+        appDir = "./app"
+    )
+}
