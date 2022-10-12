abc749479cd0d4cdae828c720031836ad40d52a7
alixlm19 <alixlm19@hotmail.com>
2022-10-12 13:02:29 -0400

    Updated server logic

--- app/server.R
+++ app/server.R
@@ -1,11 +1,32 @@
-source("./app/dependencies.R")
-source("./app/components/map_panel.R")
+source("./dependencies.R")
+source("./components/map_panel.R")
 
 server <- function(input, output) {
     output$map_output <- renderLeaflet({
-        leaflet_map
+        generate_map(input)
     })
 
-    output$map_output <- 
+    output$plot_volume_1 <- renderPlot(plot_volume_1)
+    output$plot_no_collisions_1 <- renderPlot(plot_no_collisions_1)
+    output$plot_total_injuries_1 <- renderPlot(plot_total_injuries_1)
+    output$plot_total_deaths_1 <- renderPlot(plot_total_deaths_1)
+    output$plot_injuries_per_collision_1 <- renderPlot(plot_injuries_per_collision_1)
+    output$plot_death_per_collision_1 <- renderPlot(plot_death_per_collision_1)
+
+    output$plot_volume_2 <- renderPlot(plot_volume_2)
+    output$plot_no_collisions_2 <- renderPlot(plot_no_collisions_2)
+    output$plot_total_injuries_2 <- renderPlot(plot_total_injuries_2)
+    output$plot_total_deaths_2 <- renderPlot(plot_total_deaths_2)
+    output$plot_injuries_per_collision_2 <- renderPlot(plot_injuries_per_collision_2)
+    output$plot_death_per_collision_2 <- renderPlot(plot_death_per_collision_2)
+
+    output$plot_volume_3 <- renderPlot(plot_volume_3)
+    output$plot_no_collisions_3 <- renderPlot(plot_no_collisions_3)
+    output$plot_total_injuries_3 <- renderPlot(plot_total_injuries_3)
+    output$plot_total_deaths_3 <- renderPlot(plot_total_deaths_3)
+    output$plot_injuries_per_collision_3 <- renderPlot(plot_injuries_per_collision_3)
+    output$plot_death_per_collision_3 <- renderPlot(plot_death_per_collision_3)
+
+
     output$map_label_boroughs <- map_borough_label(input)
 }
